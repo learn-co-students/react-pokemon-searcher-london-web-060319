a69b8d51db5cba855c8a274c662a70b53efd0abc
Danielle <dmiz@brandeis.edu>
2019-08-12 14:11:56 +0100

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,23 +1,74 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
-import _ from 'lodash'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import { Search } from "semantic-ui-react";
+import _ from "lodash";
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemons: [],
+    searchTerm: ""
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+      .then(resp => resp.json())
+      .then(res =>
+        this.setState({
+          pokemons: res
+        })
+      );
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value })
+  }
+  filterPokemon = () => this.state.pokemons.filter(pokemon => pokemon.name.includes(this.state.searchTerm))
+
+  // onSearchChange = event => {
+  //   const newPoke = this.state.pokemons.filter(pokemon => {
+  //     return pokemon.name.includes(event.target.value);
+  //   });
+
+  //   this.setState({
+  //     pokemons: newPoke,
+  //     pokemonSearch: event.target.value
+  //   });
+  // };
+
+
+
+  handleFormSubmit = (event, state) => {
+    event.preventDefault();
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ state }) // body data type must match "Content-Type" header
+    })
+      .then(response => response.json())
+      .then(object => {
+        this.setState({
+          pokemons: [...this.state.pokemons, object]
+        });
+      }); // parses JSON response into native JavaScript objects
+  };
+
   render() {
+    const pokemons = this.filterPokemon()
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={this.onSearchChange} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={pokemons} />
         <br />
-        <PokemonForm />
+        <PokemonForm handleFormSubmit={this.handleFormSubmit} />
       </div>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
